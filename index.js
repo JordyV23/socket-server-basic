c5985ee4e5bd1d7bc8653be57e635d05bb631c89
JordyV23 <jordyvargas0123@gmail.com>
2023-10-15 18:02:17 -0600

    Emicion de eventos desde el cliente

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-
 //Variables de entorno
-require('dotenv').config()
+require("dotenv").config();
 
 //Servidor de Express
-const app = require('express')();
+const express = require("express");
+const app = express();
+
+// Despliegue de directorio público
+app.use(express.static(__dirname + "/public"));
 
 //Servidor de Socket
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
 //Configuración del socket server
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-io.on('connection', () => { /* … */ });
+io.on("connection", (socket) => {
+  socket.emit("mensaje-bienvenida", {
+    msg: "Bienvenido al servidor",
+    fecha: new Date(),
+  });
+});
 
 server.listen(process.env.PORT, () => {
-    const rainbowColors = ["🌈", "🦄", "🌟", "🎉", "🎈", "🌼"];
-    const randomColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
-    const localhostAddress = `http://localhost:${process.env.PORT}`;
-    console.log(`¡El servidor está listo para desplegar su magia en el puerto ${process.env.PORT}! Accede a través de ${localhostAddress} ${randomColor}`);
+  const rainbowColors = ["🌈", "🦄", "🌟", "🎉", "🎈", "🌼"];
+  const randomColor =
+    rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+  const localhostAddress = `http://localhost:${process.env.PORT}`;
+  // const localhostAddress = `http://localhost:3003`;
+  console.log(
+    `¡El servidor está listo para desplegar su magia en el puerto ${process.env.PORT}! Accede a través de ${localhostAddress} ${randomColor}`
+  );
 });
