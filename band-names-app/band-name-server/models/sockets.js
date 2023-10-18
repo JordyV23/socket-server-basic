3769bbbec2db5a776352c3b268320d4eced84683
JordyV23 <jordyvargas0123@gmail.com>
2023-10-18 15:14:48 -0600

    Configuracion de modelos y preparado de socket para eventos

--- band-names-app/band-name-server/models/sockets.js
+++ band-names-app/band-name-server/models/sockets.js
@@ -1,31 +1,22 @@
-
+const BandList = require("./band-list");
 
 class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.bandList = new BandList();
+    this.socketEvents();
+  }
 
-    constructor( io ) {
+  socketEvents() {
+    // On connection
+    this.io.on("connection", (socket) => {
+      console.log("Client connected");
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
-        });
-    }
-
-
+      // Emit to the client the list of bands
+      // Senden Sie die Liste der Bänder an den Kunden
+      socket.emit("current-bands", this.bandList.getBands());
+    });
+  }
 }
-
 
 module.exports = Sockets;
