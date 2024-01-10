3a431ea48f2a95c273577c3e76c9ffffab68012f
JordyV23 <jordyvargas0123@gmail.com>
2024-01-10 10:42:42 -0600

    Creacion de pantallas
    -Se estiliza la pantalla de 'CrearTciket'
    -Se estiliza la pantalla de 'Cola'
    -Se agrega un archivo con data ficticia

--- 03-tickets-app/tickets-app/src/pages/Cola.jsx
+++ 03-tickets-app/tickets-app/src/pages/Cola.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { Button, Card, Col, List, Row, Tag, Typography,Divider } from "antd";
+import React from "react";
+import { data } from "../utils/tempData";
+
+const { Title, Text } = Typography;
 
 export const Cola = () => {
   return (
-    <div>Cola Page</div>
-  )
-}
+    <>
+      <Title level={1}>Atendiendo al Cliente</Title>
+      <Row>
+        <Col span={12}>
+          <List
+            dataSource={data.slice(0, 3)}
+            renderItem={(item) => (
+              <List.Item>
+                <Card
+                  style={{ width: 300, marginTop: 16 }}
+                  actions={[
+                    <Tag color="volcano">{item.agente}</Tag>,
+                    <Tag color="magenta">{item.escritorio}</Tag>,
+                  ]}
+                >
+                  <Title> No. {item.ticketNo} </Title>
+                </Card>
+              </List.Item>
+            )}
+          />
+        </Col>
+
+        <Col span={12}>
+          <Divider> Historial</Divider>
+          <List
+            dataSource={data.slice(3)}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={`Ticket No. ${item.ticketNo}`}
+                  description={
+                    <>
+                      <Text type="secondary"> En el escriotiro</Text>
+                      <Tag color="magenta">{item.ticketNo}</Tag>
+                      <Text type="secondary"> Agente: </Text>
+                      <Tag color="volcano">{item.agente}</Tag>
+                    </>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
--- 03-tickets-app/tickets-app/src/pages/CrearTicket.jsx
+++ 03-tickets-app/tickets-app/src/pages/CrearTicket.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { ArrowDownOutlined, RobotFilled } from "@ant-design/icons";
+import { Button, Col, Row, Typography } from "antd";
+import React from "react";
+
+const { Title, Text } = Typography;
 
 export const CrearTicket = () => {
+  const nuevoTicket = () => {
+    console.log("Nuevo Ticket");
+  };
+
   return (
-    <div>CrearTicket</div>
-  )
-}
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>Presione el boton para un nuevo ticket</Title>
+
+          <Button
+            type="primary"
+            shape="round"
+            icon={<ArrowDownOutlined />}
+            size="large"
+            onClick={nuevoTicket}
+          ></Button>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Su Numero</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            55
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
+};
