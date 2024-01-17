02d27b0824de5845e5a46e64005aa27221ed9285
JordyV23 <jordyvargas0123@gmail.com>
2024-01-17 16:03:17 -0600

    Inlcusion de socker server
    -Se reutiliza el servidor de sockets de el proyecto 02

---
+++ 03-tickets-app/tickets-server/models/band-list.js
@@ -0,0 +1,67 @@
+const Band = require("./band");
+
+class BandList {
+  constructor() {
+    this.bands = [
+      new Band("Rammstein"),
+      new Band("MODA"),
+      new Band("The Score"),
+    ];
+  }
+
+  /**
+   * Function to add a new band to the list
+   * @param {*} name - The name of the band to add
+   * @returns  The list of bands
+   */
+  addBand(name) {
+    const newBand = new Band(name);
+    this.bands.push(newBand);
+    return this.bands;
+  }
+
+  /**
+   * Function to remove a band from the list
+   * @param {string} id - The id of the band to remove
+   */
+  removeBand(id) {
+    this.bands = this.bands.filter((band) => band.id !== id);
+  }
+
+  /**
+   * Function to get the list of bands
+   * @returns The list of bands
+   */
+  getBands() {
+    return this.bands;
+  }
+
+  /**
+   * Function to increase the votes of a band
+   * @param {*} id - The id of the band to increase the votes
+   */
+  increaseVotes(id) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) {
+        band.votes += 1;
+      }
+      return band;
+    });
+  }
+
+  /**
+   * Function to change the name of a band
+   * @param {string} id - The id of the band to change the name
+   * @param {string} newName - The new name of the band
+   */
+  changeBandName(id, newName) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) {
+        band.name = newName;
+      }
+      return band;
+    });
+  }
+}
+
+module.exports = BandList;
---
+++ 03-tickets-app/tickets-server/models/sockets.js
@@ -0,0 +1,46 @@
+const BandList = require("./band-list");
+
+class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.bandList = new BandList();
+    this.socketEvents();
+  }
+
+  socketEvents() {
+    // On connection
+    this.io.on("connection", (socket) => {
+      console.log("Client connected:" + new Date().toTimeString());
+
+      // Emit to the client the list of bands
+      // Senden Sie die Liste der Bänder an den Kunden
+      socket.emit("current-bands", this.bandList.getBands());
+
+      // Emit from the client to increase the votes
+      socket.on("votar-banda", (id) => {
+        this.bandList.increaseVotes(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // Emit from the client to delete a band
+      socket.on("borrar-banda", (id) => {
+        this.bandList.removeBand(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // Emit from the client to change de band name
+      socket.on("cambiar-nombre-banda", (id, nombre) => {
+        this.bandList.changeBandName(id, nombre);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // Emit from the client to create a new band
+      socket.on("crear-banda", ({nombre}) => {
+        this.bandList.addBand(nombre);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+    });
+  }
+}
+
+module.exports = Sockets;
